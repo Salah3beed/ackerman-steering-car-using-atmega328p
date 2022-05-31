4880928cc17ada9ac9e8791089fe71d298793b2e
SalahEl-Din Ebeed <salah.aped@gmail.com>
2022-05-31 20:20:21 +0200

    Project Intialization

---
+++ XC8Application11/XC8Application11/main.c
@@ -0,0 +1,16 @@
+/*
+ * main.c
+ *
+ * Created: 5/31/2022 8:16:42 PM
+ *  Author: Salah
+ */ 
+
+#include <xc.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
