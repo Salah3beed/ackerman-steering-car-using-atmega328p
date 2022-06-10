7e6214daa1720dbf03dce8c364461d65c2aea23b
SalahEl-Din Ebeed <salah.aped@gmail.com>
2022-06-10 14:56:12 +0200

    Add: 16x2 LCD code for MC in C

---
+++ ATMEGA328P C Codes/LCD Code.c
@@ -0,0 +1,84 @@
+#define F_CPU 16000000L			/* Define CPU Frequency e.g. here 8MHz */
+#include <avr/io.h>			/* Include AVR std. library file */
+#include <util/delay.h>			/* Include inbuilt defined Delay header file */
+
+#define LCD_Data_Dir DDRD		/* Define LCD data port direction */
+#define LCD_Command_Dir DDRC		/* Define LCD command port direction register */
+#define LCD_Data_Port PORTD		/* Define LCD data port */
+#define LCD_Command_Port PORTC		/* Define LCD data port */
+#define RS PC0				/* Define Register Select (data/command reg.)pin */
+#define RW PC1				/* Define Read/Write signal pin */
+#define EN PC2				/* Define Enable signal pin */
+
+
+void LCD_Command(unsigned char cmnd)
+{
+	LCD_Data_Port= cmnd;
+	LCD_Command_Port &= ~(1<<RS);	/* RS=0 command reg. */
+	LCD_Command_Port &= ~(1<<RW);	/* RW=0 Write operation */
+	LCD_Command_Port |= (1<<EN);	/* Enable pulse */
+	_delay_us(1);
+	LCD_Command_Port &= ~(1<<EN);
+	_delay_ms(3);
+}
+
+void LCD_Char (unsigned char char_data)	/* LCD data write function */
+{
+	LCD_Data_Port= char_data;
+	LCD_Command_Port |= (1<<RS);	/* RS=1 Data reg. */
+	LCD_Command_Port &= ~(1<<RW);	/* RW=0 write operation */
+	LCD_Command_Port |= (1<<EN);	/* Enable Pulse */
+	_delay_us(1);
+	LCD_Command_Port &= ~(1<<EN);
+	_delay_ms(1);
+}
+
+void LCD_Init (void)			/* LCD Initialize function */
+{
+	LCD_Command_Dir = 0xFF;		/* Make LCD command port direction as o/p */
+	LCD_Data_Dir = 0xFF;		/* Make LCD data port direction as o/p */
+	_delay_ms(20);			/* LCD Power ON delay always >15ms */
+	
+	LCD_Command (0x38);		/* Initialization of 16X2 LCD in 8bit mode */
+	LCD_Command (0x0C);		/* Display ON Cursor OFF */
+	LCD_Command (0x06);		/* Auto Increment cursor */
+	LCD_Command (0x01);		/* Clear display */
+	LCD_Command (0x80);		/* Cursor at home position */
+}
+
+void LCD_String (const char *str)		/* Send string to LCD function */
+{
+	int i;
+	for(i=0;str[i]!=0;i++)		/* Send each char of string till the NULL */
+	{
+		LCD_Char (str[i]);
+	}
+}
+
+void LCD_String_xy (char row, char pos, char *str)/* Send string to LCD with xy position */
+{
+	if (row == 0 && pos<16)
+	LCD_Command((pos & 0x0F)|0x80);	/* Command of first row and required position<16 */
+	else if (row == 1 && pos<16)
+	LCD_Command((pos & 0x0F)|0xC0);	/* Command of first row and required position<16 */
+	LCD_String(str);		/* Call LCD string function */
+}
+
+void LCD_Clear()
+{
+	LCD_Command (0x01);		/* clear display */
+	LCD_Command (0x80);		/* cursor at home position */
+}
+
+int main()
+{
+
+
+	LCD_Init();			/* Initialize LCD */
+	
+	LCD_String("Yarab nkhls");	/* write string on 1st line of LCD*/
+	LCD_Command(0xC0);		/* Go to 2nd line*/
+	LCD_String("Amen");	/* Write string on 2nd line*/
+
+	return 0;
+}
